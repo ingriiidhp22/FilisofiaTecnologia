b4f3012468548c12f78a321b8bc7d2e9b23f01a1
ingriiidhp22 <126804688+ingriiidhp22@users.noreply.github.com>
2024-02-01 00:40:41 +0930

    Update sketch.js

--- hello-ml5/sketch.js
+++ hello-ml5/sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/gato14.png');
+  img = loadImage('images/gato03.png');
 }
 
 function setup() {
